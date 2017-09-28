58a0521d267b92a8d7e2936fe0b323d8afef65b4
Akshath Jain <akshath.r.jain@gmail.com>
2017-09-27 20:22:58 -0400

    completed open source projects binding

--- scripts/githubScripts.js
+++ scripts/githubScripts.js
@@ -5,13 +5,39 @@ Purpose: github api handler
 */
 
 $(document).ready(function(){
-	var request = new XMLHttpRequest();
-	request.onload = parser;
-	request.open('get', 'https://api.github.com/users/akshathjain/events', true);
-	request.send();
-
-	function parser(){
+	
+	//get the research project data
+	retrieveGithubData('https://api.github.com/users/akshathjain/repos', function(){
 		var response = JSON.parse(this.responseText);
 		console.log(response);
-	}
+		layoutInflator(response, 'open-source-projects-layout', 'open-source-projects-container');
+	});
+
+	
 });
+
+//function to request data from github api
+function retrieveGithubData(url, onComplete){
+	var request = new XMLHttpRequest();
+	request.onload = onComplete;
+	request.open('get', url, true);
+	request.send();
+}
+
+function layoutInflator(data, template, holder, binder){
+	//populate the data	
+	var layout = document.getElementById(template);
+	for(var i = 0; i < data.length; i++){
+		var layoutClone = layout.cloneNode(true);
+		layoutClone.id = template + "-" + i;
+
+		var title = layoutClone.getElementsByTagName("p")[0]	;
+		var description = layoutClone.getElementsByTagName("span")[1];
+
+		title.innerHTML = data[i].name;
+		description.innerHTML = data[i].description;
+
+		document.getElementById(holder).appendChild(layoutClone);
+	}
+	layout.style.display = "none"; //hide the layout template
+}
