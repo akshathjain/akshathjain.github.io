8b231a846ba22ba59c4461525bbf4ac7f9f35868
Akshath Jain <akshath.r.jain@gmail.com>
2017-11-02 19:34:05 -0400

    uploaded skeleton

--- scripts/githubScripts.js
+++ scripts/githubScripts.js
@@ -58,18 +58,3 @@ function retrieveGithubData(url, onComplete){
 	request.open('get', url, true);
 	request.send();
 }
-
-//function to bind data to layout
-function layoutInflator(data, template, holder, binder){
-	//populate the data	
-	var layout = document.getElementById(template);
-	for(var i = 0; i < data.length; i++){
-		var layoutClone = layout.cloneNode(true);
-		layoutClone.id = template + "-" + i;
-
-		binder(data, i, layoutClone);
-
-		document.getElementById(holder).appendChild(layoutClone);
-	}
-	layout.style.display = "none"; //hide the layout template
-}
