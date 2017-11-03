283a6f3faee2ccb74d733ea963c3b7a2a819166e
Akshath Jain <akshath.r.jain@gmail.com>
2017-11-02 22:54:52 -0400

    finished data binding (for the most part)

--- scripts/leadershipScripts.js
+++ scripts/leadershipScripts.js
@@ -8,12 +8,30 @@ $(document).ready(function(){
 
 	//load JSON
 	$.getJSON('http://akshathjain.com/json/leadership.json', function(response){
-		console.log(response);
-		layoutInflator(response.leadership, 'leadership-card-layout', 'leadership-card-layout-container', function(data, i, layout){
-			console.log('getting here');
-			var richMedia = layout.getElementsByTagName('img');
-			console.log(richMedia[0]);
+		console.log(response.leadership);
+		console.log(document.getElementById('leadership-card-container'));
 
+		layoutInflator(response.leadership, 'leadership-card-layout', 'leadership-card-container', function(data, i, layout){
+			var title = layout.getElementsByTagName("h3")[0];
+			var richMedia = layout.getElementsByTagName('img')[0];
+			var infoList = layout.getElementsByTagName("ul")[0];
+			var listItem = infoList.getElementsByTagName("li")[0];
+
+			title.innerHTML = data[i].title;
+			richMedia.src = data[i].richVisual;
+
+			for(var j = 0; j < data[i].info.length; j++){
+				var listLayout = listItem.cloneNode(true);
+				var listTitle = listLayout.getElementsByTagName("p")[0];
+				var listContent = listLayout.getElementsByTagName("p")[1];
+
+				listTitle.innerHTML = data[i].info[j].title;
+				listContent.innerHTML = data[i].info[j].content;
+
+				infoList.appendChild(listLayout);
+			}
+
+			listItem.style.display = "none";
 		});
 
 		//call the footer resizer function to update status after content has been populated
